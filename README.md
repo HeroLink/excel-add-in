4a527951e78fef02fb6573059c276a74a581b497
Link Chen <linkch@qq.com>
2022-03-22 20:17:41 +0800

    init repo

---
+++ README.md
@@ -0,0 +1,2 @@
+# Excel Add-In
+1. [API](https://docs.microsoft.com/en-us/javascript/api/excel?view=excel-js-1.14)
---
+++ src/taskpane/taskpane.ts
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+/* global console, document, Excel, Office */
+
+Office.onReady((info) => {
+  if (info.host === Office.HostType.Excel) {
+    document.getElementById("sideload-msg").style.display = "none";
+    document.getElementById("app-body").style.display = "flex";
+    document.getElementById("run").onclick = run;
+  }
+});
+
+export async function run() {
+  try {
+    await Excel.run(async (context) => {
+      /**
+       * Insert your Excel code here
+       */
+      // const range = context.workbook.getSelectedRange();
+      const range = context.workbook.worksheets.getActiveWorksheet().getRange("F2:H4");
+
+      // Read the range address
+      // range.load("address");
+      range.load("values");
+
+      // Update the fill color
+      range.format.fill.color = "#00ffcc";
+
+      await context.sync();
+      // console.log(`The range address was ${range.address}.`);
+      console.log(`The range address was ${range.values}.`);
+      range.values = [
+        ["Hi", "Bojre", "Karlsoon"],
+        ["I am", "Chen", "Lu"],
+        ["You can", "call me", "Link Chen"],
+      ];
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
